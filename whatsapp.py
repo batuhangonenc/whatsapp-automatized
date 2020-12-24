f8c143745b552bcbedd2d903f46fc2f879fce8ec
batuhangonenc <gonencbthn@gmail.com>
2020-12-24 03:09:41 +0300

    improved by regex controls

--- whatsapp.py
+++ whatsapp.py
@@ -1,45 +1,78 @@
-import pywhatkit
+import pywhatkit,re
+from datetime import datetime as dt
 
 receivers = []
+
 
 print("\n\n\nfor stop the loop 'q'.\n\n\n")
 
 while 1:
+	
+	pattern=r"^\+[0-9]* [0-9][0-9][0-9] [0-9][0-9][0-9] [0-9][0-9] [0-9][0-9]$"
 
-	num = input("enter a phone number (ex --> +90 xxx xxx xx xx) :")
+	num = input("enter a phone number (ex -> +90 xxx xxx xx xx) :")
+
+	match = re.match(pattern,num)
+
+	if not(match) and not(num == "q"):
+		print("\nunvalid input type.")
+		continue
+
+
+	elif num == "q":
+		break
 
 	generalstr = ""
+
 	for i in receivers:
 		generalstr += i
 
-	if num == "q":
-		break
+
+	if generalstr.count(num) == 0:
+
+		message = input(f"enter your message to {num} :")
+
+		while 1:
+			pat2 = r"^[0-9]{1,2}"
+			
+			hour = input("enter a hour :")
+
+			match = re.match(pat2,hour)
+
+			if not(match):
+				print("\nunvalid input type.")
+				continue
+
+
+
+			minute = input("enter a minute :")
+			match = re.match(pat2,minute)
+
+			if not(match):
+				print("\nunvalid input type")
+				continue
+
+			break
+
 		
-	elif generalstr.count(num) == 0:
-		
-		hour = int(input("enter a hour :"))
-		
-		minute = int(input("enter minute:"))
-		
-		message = input("enter your message :")
 
 		content = f"{num}$@${message}$@${hour}$@${minute}"
 
 		receivers.append(content)
 
 	else:
-		print("this phone num is already in list.\n\n")
-		continue
+		print("--\nthis phone num is already in list.\n")
 
 
+for messages in receivers:
+	data = messages.split("$@$")
 
-
-for num in receivers:
 	try:
-		data = num.split("$@$")
-		pywhatkit.sendwhatmsg(data[0],data[1],data[2],data[3])
-		print(f"\nmessage sent : {num}")
-	except:
-		print("***\nsomething went wrong.")
 
-	
+		pywhatkit.sendwhatmsg(data[0],data[1],int(data[2]),int(data[3]))
+		print(f"---\nmessage sent successfully : {data[0]}")
+
+
+	except:
+		print(f"--\nsomething went wrong : {data[0]}.")
+
